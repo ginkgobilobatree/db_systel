25009ad2da9abf1578ec60cdfc8236b84d73f47b
Gino Massalski <gino.massalski@gmail.com>
2022-11-20 21:21:53 +0100

    structure file system

--- utils/createDS100File.js
+++ utils/createDS100File.js
@@ -3,12 +3,10 @@ const { parse } = require("csv-parse");
 
 const DS100 = {};
 
-let num = 0;
-fs.createReadStream("./files/D_Bahnhof_2020_alle.csv")
+fs.createReadStream("../data/D_Bahnhof_2020_alle.csv")
   .pipe(parse({ delimiter: ";", from_line: 2 }))
   .on("data", function (row) {
     if (row[4] === "FV") {
-      num += 1;
       DS100[row[1]] = {
         lang: row[5],
         lat: row[6],
@@ -20,8 +18,8 @@ fs.createReadStream("./files/D_Bahnhof_2020_alle.csv")
     console.log(error.message);
   })
   .on("end", function () {
-    const data = JSON.stringify(DS100)
-    fs.writeFileSync("DS100.json", data);
-    console.log(data)
+    const data = JSON.stringify(DS100);
+    fs.writeFileSync("../data/DS100.json", data);
+    console.log(data);
     console.log("it's done");
   });
