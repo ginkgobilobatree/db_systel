03e02357badca6d0fb66a4da7a1d78f3284f54ef
Gino Massalski <gino.massalski@gmail.com>
2022-11-21 12:05:00 +0100

    send json object

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const api = '/api/v1/distance';
 const DS100 = require("./data/DS100.json");
 
 app.get(api, (req, res) => {
-	res.send(`{°O.o°}`);
+	res.send({"here": "result"});
 })
 app.get(api + '/:station1/:station2', (req, res) => {
-	const station1 = req.params.station1;
-	const station2 = req.params.station2;
-	res.send([DS100[station1], DS100[station2]]);
+	res.send({
+		"from": `${DS100[req.params.station1]?.name}`,
+		"to": `${DS100[req.params.station2]?.name}`,
+		"distance":"in progress",
+		"unit": "km"
+	})
 })
 app.get('/*', (req, res) => {
 	res.send(`wrong address! type localhost:${port}${api}`);
