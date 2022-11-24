b23adb577e1c40ede58d835fc9b6e01fd760c3e3
Gino Massalski <gino.massalski@gmail.com>
2022-11-24 13:58:06 +0100

    add endpoint, change condition for endpoint

--- modules/controller.js
+++ modules/controller.js
@@ -4,18 +4,27 @@ const port = process.env.PORT || 3001;
 const DS100 = require("../data/DS100.json");
 
 const Middleware = require("./middleware");
+
 const endpoint = "/api/v1/distance";
 
+const invalidInput = {
+  from: "invalid input",
+  to: "invalid input",
+  distance: "invalid input",
+  unit: "invalid input",
+};
+
+
 router.get(endpoint, (req, res) => {
-  res.send({ "this is ": "a test" });
+  res.send({ "this test": "is working :-)" });
 });
 
 router.get(endpoint + "/:station1/:station2", (req, res) => {
   const stat1 = req.params.station1;
   const stat2 = req.params.station2;
 
-  if (!(stat1 in DS100) || !(stat2 in DS100))
-    res.send({ "were your entries valid?": "NOPE!" });
+  if (!(stat1 in DS100) || !(stat2 in DS100 || stat1.length === 0 || stat2.length === 0))
+    res.send(invalidInput);
 
   const [lat1, lat2, long1, long2] = [
     DS100[stat1].lat,
@@ -34,8 +43,13 @@ router.get(endpoint + "/:station1/:station2", (req, res) => {
   });
 });
 
-router.get("/*", (req, res) => {
-  res.send(`wrong address! type localhost:${port}${endpoint}`);
+router.get(endpoint + "/*", (req, res) => {
+  res.send(invalidInput);
 });
+
+router.get("/*", (req, res) => {
+  res.send("try 'localhost:3000/api/v1/distance'");
+});
+
 
 module.exports = router;
